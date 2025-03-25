78a910ec2bf99ec33a8aff2cbb40ae37925a164d
Kavindya Sithumini <167311420+kavindyaaa@users.noreply.github.com>
2025-03-25 18:07:44 +0530

    add expence, update, delete and view frontend
    add expense, update, delete and view frontend functionalities after conflicts resolved

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,35 +1,21 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-<<<<<<<<< Temporary merge branch 1
+
+import AllExpences from './pages/expence/AllExpence'
+import Expence from './pages/expence/Expence'
+
 import AllServices from '../src/pages/services/AllServices'
 import AddService from '../src/pages/services/AddService'
 import Service from '../src/pages/services/Service'
 import AddSchedule from '../src/pages/shedule/AddSchedule'
 import AllShedules from '../src/pages/shedule/AllShedule'
 import Shedule from '../src/pages/shedule/Shedule'
-import AddExpence from './pages/AddExpence'
 import AddInventory from './pages/AddInventory'
 import AddNotification from './pages/notifications/AddNotification'
 import AllNotification from './pages/notifications/AllNotifications'
 import Notification from './pages/notifications/Notification'
+import AddExpence from './pages/expence/AddExpence'
 
-=========
-import AllServices from './pages/services/AllServices'
-import AddService from './pages/services/AddService'
-import Service from './pages/services/Service'
-import AddSchedule from './pages/AddSchedule'
-import AddInventory from './pages/AddInventory'
-import  AddExpence from './pages/expence/AddExpence'
-import AllExpences from './pages/expence/AllExpence'
-import Expence from './pages/expence/Expence'
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
->>>>>>>>> Temporary merge branch 2
 function App() {
 
   return (
@@ -40,7 +26,10 @@ function App() {
         <Route path="/service/all-services" element={<AllServices />} />
         <Route path="/service/:id" element={<Service />} />
         <Route path='/add-schedule' element={<AddSchedule />} />
-<<<<<<<<< Temporary merge branch 1
+
+      <Route path='/all-expences' element={<AllExpences />} />
+      <Route path='/expence/:id' element={<Expence />} />
+
         <Route path='/all-shedules' element={<AllShedules />}/>
         <Route path='/displayShedule/:id' element={<Shedule />}/>
         <Route path='/add-expence' element={<AddExpence />} />
@@ -48,12 +37,7 @@ function App() {
         <Route path="/notification/all-notification" element={<AllNotification />} />
         <Route path="/notification/add-notification" element={<AddNotification />} />
         <Route path="/notification/:id" element={<Notification />} />
-=========
-        <Route path='/add-inventory' element={<AddInventory />} />
-        <Route path='/add-expence' element={<AddExpence />} />
-      <Route path='/all-expences' element={<AllExpences />} />
-      <Route path='/expence/:id' element={<Expence />} />
->>>>>>>>> Temporary merge branch 2
+
       </Routes>
     </div>
   )
